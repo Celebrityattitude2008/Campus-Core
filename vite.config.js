ba72b66869fbfbdf09f5eb2ea3eac50c8a2d4667
Paul Adamu <pauladamu600@gmail.com>
2026-05-04 13:51:54 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+
+const pageFiles = readdirSync(new URL('.', import.meta.url))
+  .filter(file => file.endsWith('.html'))
+  .reduce((pages, file) => {
+    const name = file === 'index.html' ? 'index' : file.replace('.html', '')
+    pages[name] = resolve(new URL(file, import.meta.url).pathname)
+    return pages
+  }, {})
 
 export default defineConfig({
   root: '.',
@@ -9,6 +19,9 @@ export default defineConfig({
     strictPort: false
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: pageFiles
+    }
   }
 })
